4324bbd2cb85aeda987219096964ddb503a0c22a
Yaolegol <m160160@yandex.ru>
2024-12-14 17:19:28 +0700

    Доработан CatalogController для использования локали

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -12,7 +12,11 @@ class CatalogController extends Controller
      */
     public function index(Request $request)
     {
-        $query = Product::with(['translations', 'categories']);
+        $locale = $request->get('locale', 'ru');
+
+        $query = Product::with(['translations' => function ($query) use ($locale) {
+            $query->where('locale', $locale);
+        }, 'categories']);
 
         if ($request->has('category')) {
             $query->whereHas('category', function ($q) use ($request) {
